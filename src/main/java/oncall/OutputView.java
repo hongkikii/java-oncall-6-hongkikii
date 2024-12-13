007f4d401890592deff360d198dc219ea9e851db
mimi <110226866+hongkikii@users.noreply.github.com>
2024-12-13 17:13:19 +0900

    fix(OutputView): 근무표 출력 시 개행 출력 추가

--- src/main/java/oncall/OutputView.java
+++ src/main/java/oncall/OutputView.java
@@ -3,7 +3,8 @@ package oncall;
 public class OutputView {
     public void show(WorkTable workTable) {
         for(WorkInfo workInfo : workTable.getValue()) {
-            System.out.println(workInfo.getFormatted());
+            System.out.println();
+            System.out.print(workInfo.getFormatted());
         }
     }
 }
